cc16859fb5799ff28a9ae2fd1a4d724ade6dad80
Kziete <kziete@gmail.com>
2015-08-16 19:30:54 -0300

    Integer Field

---
+++ core/Modelo/Widgets/IntegerModel.php
@@ -0,0 +1,22 @@
+<?php namespace Modelo\Widgets;
+
+use Modelo\Widgets\WidgetPadre;
+
+class IntegerModel extends WidgetPadre{
+	public $max_length;
+	public function __construct($hash){
+		parent::__construct($hash);
+		$this->max_length = $hash['max_length'] ? $hash['max_length'] : 128;
+	}
+	public function getInput($campo=null,$value=null){
+		$hash = array(
+			'placeholder' => $campo,
+			'name' => $campo,
+			'value' => $value
+		);	
+		return parent::input($hash);
+	}
+	public function getFieldType(){
+		return "bigint";
+	}
+}
